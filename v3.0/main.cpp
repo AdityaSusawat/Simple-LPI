46f937871c560ee8e9431db3e540a1e1e2ee64cc
Aditya Susawat <adityaks@iitbhilai.ac.in>
2021-08-11 20:08:17 +0530

    Update main.cpp

--- v3.0/main.cpp
+++ v3.0/main.cpp
@@ -1,1 +1,26 @@
+//Work in progress
 
+#include "lexer.hpp"
+#include <iostream>
+
+
+int main() try {
+
+    Lexer lex{std::cin};
+    while (lex) {
+        auto tok = lex.extract();
+        auto pos = lex.get_position();
+        std::cout << pos << " " << tok << "\n";
+    }
+
+}
+
+catch (std::exception& e) {
+    std::cerr << "Error: " << e.what() << "\n";
+    return -1;
+}
+
+catch (...) {
+    std::cerr << "Unknown error.\n";
+    return -1;
+}
